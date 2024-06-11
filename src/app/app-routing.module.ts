788e6f69ce6abdd0aa1c86ba8b0f440eb660599f
SAKTHIARUNP <sakthiprabhu1128@gmail.com>
2024-06-11 18:06:22 +0530

    success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LeadComponent } from './lead/lead.component';
+import { LeadViewComponent } from './leadview/leadview.component';
+import { LeadEditComponent } from './lead-edit/lead-edit.component';
+import { ApprovalComponent } from './approval/approval.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'lead', component: LeadComponent },
+  { path: 'leadview/:id', component: LeadViewComponent },
+  { path: 'lead-edit/:id', component: LeadEditComponent },
+  { path: 'approval/:id', component: ApprovalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LeadComponent } from './lead/lead.component';
+import { LeadViewComponent } from './leadview/leadview.component';
+import { ApprovalComponent } from './approval/approval.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ClassComponent } from './class/class.component';
+import { HomeComponent } from './home/home.component';
+import { LeadEditComponent } from './lead-edit/lead-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LeadComponent,
+    LeadViewComponent,
+    ApprovalComponent,
+    ClassComponent,
+    HomeComponent,
+    LeadEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
